330361ccbc7671811dfc9da8ebf30b485bbb48c4
Marcelle von Wendland <mvw@hivves.com>
2015-06-18 15:15:00 +0100

    start

--- httpsServer.go
+++ httpsServer.go
@@ -1,15 +1,28 @@
 package main
+
+
 import (
+	"fmt"
 	"log"
-	"net/http"
+	"net/http"	
 )
 
+const (
+    PORT       = ":8443"
+    PRIV_KEY   = "key.pem"
+    PUBLIC_KEY = "cert.pem"
+)
+
+func rootHander(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Nobody should read this.")
+}
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("/home/dekcom/Repository/website/i-aor.com")))
-	log.Println("Server starting")
-	err := http.ListenAndServeTLS(":443", "/home/dekcom/Repository/HttpsServ/cert.pem", "/home/dekcom/Repository/HttpsServ/key.pem", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+    http.HandleFunc("/", rootHander)
+    log.Println("Server starting")  
+    err := http.ListenAndServeTLS(PORT, PUBLIC_KEY, PRIV_KEY, nil)
+    if err != nil {
+        fmt.Printf("main(): %s\n", err)
+        		log.Fatal(err)
+    }
 }
